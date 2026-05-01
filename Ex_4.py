06e81d923d458a498b3567804a79d58f714f72aa
GMBENAC <benacmgabriel@gmail.com>
2026-04-30 21:37:37 -0300

    Rename exercicio4.py to Ex_4.py

---
+++ Ex_4.py
@@ -0,0 +1,3 @@
+n1 = float(input('Digite um número:'))
+raizq = (n1)**(1/2)
+print('A raiz quadrada de {} é igual a: {}'.format(n1, raizq))
--- exercicio4.py
+++
@@ -1,3 +0,0 @@
-n1 = float(input('Digite um número:'))
-raizq = (n1)**(1/2)
-print('A raiz quadrada de {} é igual a: {}'.format(n1, raizq))
